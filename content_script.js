197924331d7e685d8b411e5aa05ce691f10f7b61
udidoron <ehud.doron@mail.huji.ac.il>
2014-07-01 10:41:04 +0300

    Added resize check for lint

--- content_script.js
+++ content_script.js
@@ -19,8 +19,16 @@ wrapperElement.style.resize = 'both';
 wrapperElement.style.overflow = 'auto';
 wrapperElement.draggable = true;
 
+wrapperElement.addEventListener('dragend', function(evt) {
+	evt.preventDefault();
+	console.log("X: "+evt.pageX, "Y: "+evt.pageY);
+	wrapperElement.style.top = evt.pageY;
+	wrapperElement.style.left = evt.pageX;
+	console.log("Top: "+wrapperElement.style.top, "Left: "+wrapperElement.style.left);
+});
+
 function cssLint(elem) {
-	var strategies = [directionsWithPositionStatic, clearfix];
+	var strategies = [directionsWithPositionStatic, clearfix, resizeWithoutOverflow];
 	var style = window.getComputedStyle(elem);
 
 	var result =  strategies.reduce(function(msg, strategy) {
@@ -63,10 +71,17 @@ function directionsWithPositionStatic(obj) {
 		return result || (obj.style[direction] !== 'auto')
 	}, false);
 	if (isDirectionSet && obj.style.position === 'static') {
-		return 'Directions are given, but position is static';
+		return 'Coordinates and/or z-index are given, but position is static';
 	}
 
 	return '';
+}
+
+
+function resizeWithoutOverflow(obj) {
+	var resize = (obj.style["resize"] !== "none");
+	var noOverflow = (obj.style["overflow"] === "visible" || obj.style["overflow"] === "");
+	return (resize && noOverflow)? "Resize option is given without fitting overflow element" : "";
 }
 
 //# sourceURL=content_script.js
